6b8750cd98f6beca6c6712906f95350dfe15832d
erica <dv24end@cs.umu.se>
2025-05-10 14:30:45 +0200

    Added pan camera to right function

--- main.gd
+++ main.gd
@@ -1,7 +1,23 @@
 extends Node
 
 var screen_size
+var click_count = 0
+			
+#Move camera 1915 px to the right
+func pan_camera_right():
+	var camera = $Camera2D
+	var new_position = camera.position + Vector2(1915, 0) 
+	camera.position = new_position
 
+#Function to run tutorial
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.pressed:
-		$Customer1.is_moving = true
+		click_count +=1
+		#Enter first customer
+		if click_count == 1:
+			$Customer1.is_moving = true
+		#Pan camera to the right
+		elif click_count == 2:
+			pan_camera_right()
+	
+	
