30d9588849a2cb3df3baaa6422a8fd902a69032d
erica <dv24end@cs.umu.se>
2025-05-10 17:26:12 +0200

    witch dialogue 1

--- main.gd
+++ main.gd
@@ -1,7 +1,16 @@
 extends Node
-
+#[color=#271415][font_size=40]
 var screen_size
 var click_count = 0
+var current_line = 0
+
+var witch_lines = [
+	"[color=#271415][font_size=40]Witch: Hey! I am looking for someone to fix my broken jar.",
+	"[color=#271415][font_size=40]An imbecile in a trenchcoat bumped into me yesterday and now the jar is in a thousand pieces!",
+	"[color=#271415][font_size=40]Bah stupid cats, they never look where they are walking - always clumsy and…",
+	"[color=#271415][font_size=40]oh I am so sorry, I really didn’t mean that, I LOOOVE CATS!",
+	"[color=#271415][font_size=40]Anyways, really need this jar restored, think you can fix it? "
+]
 			
 #Move camera 1915 px to the right
 func pan_camera_right():
@@ -15,6 +24,9 @@ func pan_camera_left():
 	var new_position = camera.position - Vector2(1915, 0) 
 	camera.position = new_position
 	
+func witch_chat():
+	pass
+	
 #Function to run tutorial
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.pressed:
@@ -22,10 +34,35 @@ func _unhandled_input(event):
 		#Enter first customer
 		if click_count == 1:
 			$Customer1.is_moving = true
-		#Pan camera to the right
+			
 		elif click_count == 2:
+			$ChatWitch/WitchChatBox.visible = true
+			$ChatWitch/WitchChatBox/Witch_1.text = witch_lines[current_line]
+		
+		elif click_count == 3:
+			current_line +=1
+			$ChatWitch/WitchChatBox/Witch_1.text = witch_lines[current_line]
+		
+		elif click_count == 4:
+			current_line +=1
+			$ChatWitch/WitchChatBox/Witch_1.text = witch_lines[current_line]
+		
+		elif click_count == 5:
+			current_line +=1
+			$ChatWitch/WitchChatBox/Witch_1.text = witch_lines[current_line]
+		
+		elif click_count == 6:
+			current_line +=1
+			$ChatWitch/WitchChatBox/Witch_1.text = witch_lines[current_line]	
+		
+			
+		#Pan camera to the right
+		elif click_count == 7:
 			pan_camera_right()
-		elif click_count == 3: 
+			$ChatWitch/WitchChatBox.visible = false  # ✅ Korrekt sökväg
+
+		elif click_count == 8: 
 			pan_camera_left()
+			
 	
 	
