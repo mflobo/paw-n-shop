485b19fffa7dd7f08610f71c5f3ea6652053c893
erica <dv24end@cs.umu.se>
2025-05-11 11:45:16 +0200

    Jar falling

--- jar.gd
+++ jar.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 
-var fall_time = 3.0  # Tiden för att objektet ska falla (i sekunder)
+var fall_time = 1.0  # Tiden för att objektet ska falla (i sekunder)
 var timer = 0.0  # Tidsräknare för att hålla koll på när objektet ska sluta falla
 
 func _ready():
@@ -14,4 +14,5 @@ func _process(delta):
 	# Om timern har passerat den angivna tiden
 	if timer >= fall_time:
 		# Sätt objektet till Static så att det slutar falla
+		freeze = true
 		gravity_scale = 0
